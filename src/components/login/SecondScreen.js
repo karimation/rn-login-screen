ef473ddbd497d7d5a0aee5cb922570dfed18cdcb
Selmi11895480 <selmi.kerim@gmail.com>
2018-05-14 17:38:15 +0100

    logout screen

--- src/components/login/SecondScreen.js
+++ src/components/login/SecondScreen.js
@@ -7,6 +7,7 @@ import {
   TouchableOpacity,
   Animated,
   Easing,
+  Text,
 } from 'react-native';
 import {Actions, ActionConst} from 'react-native-router-flux';
 
@@ -50,6 +51,7 @@ export default class SecondScreen extends Component {
 
     return (
       <View style={styles.container}>
+        <Text>Welcome Mr Blabla</Text>
         <TouchableOpacity
           onPress={this._onPress}
           style={styles.button}
