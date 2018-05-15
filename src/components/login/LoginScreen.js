200a71112917d4022d8ff300c8d9d0873bd7e889
Selmi11895480 <selmi.kerim@gmail.com>
2018-05-15 10:57:54 +0100

    KeyboardAvoidingView update

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {KeyboardAvoidingView} from 'react-native';
 import PropTypes from 'prop-types';
 import Logo from './Logo';
 import Form from './Form';
@@ -9,12 +10,14 @@ import SignupSection from './SignupSection';
 export default class LoginScreen extends Component {
   render() {
     return (
+      <KeyboardAvoidingView>
       <Wallpaper>
         <Logo />
         <Form />
         <SignupSection />
         <ButtonSubmit />
       </Wallpaper>
+      </KeyboardAvoidingView>
     );
   }
 }
